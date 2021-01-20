9ce031b61d91a20c3fec9a70fb9fb955eda3dedd
Ricardo Arévalo <63374065+ricardocode@users.noreply.github.com>
2021-01-20 18:53:39 -0500

    feature: rediseño

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
     $('.carousel').carousel()
-    $('body').scrollspy({ target: '#list-example' })
 });
 
 try {
     $(window).scroll(function (event) {
         var scroll = $(window).scrollTop(); // Identifico el valor de scroll
         
-        if (scroll > 90) { //Si el scroll es superior a 90px el header se hace fijo
-            $('header').addClass('fixedHeader');
-            $('img.ricardo-logo-w').addClass('showLogo');
-            $('img.ricardo-logo').removeClass('showLogo');
+        if (scroll > 500) { //Si el scroll es superior a 90px el header se hace fijo
+            $('header').addClass('header__color');
+
             
             
         } else {// Al volver al top, el header se normaliza
-            $('header').removeClass('fixedHeader');
-            $('img.ricardo-logo-w').removeClass('showLogo');
-            $('img.ricardo-logo').addClass('showLogo');
-        }
-        //Función para fijar la navegación vertical de scrollSpy
-        if (scroll > 535) {
-            $('.spyBar').addClass('fixedBar');
+            $('header').removeClass('header__color');
+            
 
-        } else {
-            $('.spyBar').removeClass('fixedBar')
+          
+        }
+
+        if (scroll > 80) { //Si el scroll es superior a 90px el header se hace fijo
+            $('.ricardo-logo').addClass('showLogo');    
+            
+        } else {// Al volver al top, el header se normaliza
+            $('.ricardo-logo').removeClass('showLogo');
+
         }
     });
     } catch (error) {
